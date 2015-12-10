c2e99fc8dcee626317baa165a783082e95fd69b5
Deadcreep <tolla.von@gmail.com>
2015-12-10 18:14:52 +0500

    1213

--- index.php
+++ index.php
@@ -7,32 +7,11 @@
         <title>Table Bootstrap</title>
     </head>
     <body>
-          <table>
-      <thead>
-        <tr>
-          <th>Заголовок 1</th>
-          <th>Заголовок 2</th>
-          <th>Заголовок 3</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td rowspan="3">Содержимое ячейки</td>
-          <td colspan="2">Содержимое ячейки</td>
-        </tr>
-        <tr>
-          <td>Содержимое ячейки</td>
-          <td>Содержимое ячейки</td>
-        </tr>
-        <tr>
-          <td>Содержимое ячейки</td>
-          <td rowspan="2">Содержимое ячейки</td>
-        </tr>
-        <tr>
-          <td colspan="2">Содержимое ячейки</td>
-        </tr>
-      </tbody>
-    </table>
+        <div id="table">
+            <form action="TableBootstrap.php">
+              <button type="submit" class="btn btn-info">Таблица</button>
+            </form>
+        </div>
     </body>
 </html>
 
